9d37b5f2a9c378e117168e88b0298c31b5c249f9
Ihor Shashlo <igor.shashlo@gmail.com>
2023-02-08 12:57:52 +0200

    HW

---
+++ index.js
@@ -0,0 +1,12 @@
+        {let a = +prompt("First number?", "");
+        let b = +prompt("Second numbet?", "");
+        alert(a+b);}
+        {let a = +prompt("First number?", "");
+        let b = +prompt("Second numbet?", "");
+        alert(a-b);}
+        {let a = +prompt("First number?", "");
+        let b = +prompt("Second numbet?", "");
+        alert(a*b);}
+        {let a = +prompt("First number?", "");
+        let b = +prompt("Second numbet?", "");
+        alert(a/b);}
